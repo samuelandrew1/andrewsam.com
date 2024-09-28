d0e542530310cf0fe6e94b14743d4b9854381495
concept-technology <andrewsamuelcloud@gmail.com>
2024-09-28 22:44:12 +0100

    checking out for errrors

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-     // Use './' for relative paths or specify the subdirectory if needed
 })
