800e64d0d8f292134343d00bcdb26323558e8787
concept-technology <andrewsamuelcloud@gmail.com>
2024-09-28 23:55:51 +0100

    changed carosel

--- webapp/src/Home-page/components/Home-App.tsx
+++ webapp/src/Home-page/components/Home-App.tsx
@@ -5,7 +5,7 @@ const HomeApp = () => {
 
     return (
 <>
- <header className="header pos-re slider-fade" data-scroll-index="0">
+ {/* <header className="header pos-re slider-fade" data-scroll-index="0">
 <div className="owl-carousel owl-theme">
     <div className="item bg-img" data-overlay-dark="5" data-background="src/assets/img/bg4.jpg">
         <div className="text-center v-middle caption mt-30">
@@ -50,7 +50,64 @@ const HomeApp = () => {
         </div>
     </div>
 </div>
-</header>
+</header> */}
+
+
+<div id="carouselExampleCaptions" className="carousel slide" data-mdb-ride="carousel" data-mdb-carousel-init>
+  <div className="carousel-indicators">
+    <button
+      type="button"
+      data-mdb-target="#carouselExampleCaptions"
+      data-mdb-slide-to="0"
+      className="active"
+      aria-current="true"
+      aria-label="Slide 1"
+    ></button>
+    <button
+      type="button"
+      data-mdb-target="#carouselExampleCaptions"
+      data-mdb-slide-to="1"
+      aria-label="Slide 2"
+    ></button>
+    <button
+      type="button"
+      data-mdb-target="#carouselExampleCaptions"
+      data-mdb-slide-to="2"
+      aria-label="Slide 3"
+    ></button>
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp" className="d-block w-100" alt="Wild Landscape"/>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </div>
+    </div>
+    <div className="carousel-item">
+      <img src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp" className="d-block w-100" alt="Camera"/>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+    <div className="carousel-item">
+      <img src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp" className="d-block w-100" alt="Exotic Fruits"/>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </div>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-mdb-target="#carouselExampleCaptions" data-mdb-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-mdb-target="#carouselExampleCaptions" data-mdb-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
     <div className="why-us section-padding bg-gray pos-re">
     <div className="container">
         <div className="row">            
