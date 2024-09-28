f8ad6ea55678a7834d8cb1d3c6859a510e28a9e8
concept-technology <andrewsamuelcloud@gmail.com>
2024-09-28 22:21:08 +0100

    added vite setting

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: './', // Use './' for relative paths or specify the subdirectory if needed
+    base: 'src/assets', // Use './' for relative paths or specify the subdirectory if needed
 })
