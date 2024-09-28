72feece0819537817820d62e53185118f15522a8
concept-technology <andrewsamuelcloud@gmail.com>
2024-09-28 22:17:13 +0100

    set up vite

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: './', // Use './' for relative paths or specify the subdirectory if needed
 })
