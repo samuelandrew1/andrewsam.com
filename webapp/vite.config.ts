aaa12e98bbe9e83b6b69f03a1767536a8bc89ddc
concept-technology <andrewsamuelcloud@gmail.com>
2024-09-28 22:34:59 +0100

    still checking

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: 'src/assets', // Use './' for relative paths or specify the subdirectory if needed
+     // Use './' for relative paths or specify the subdirectory if needed
 })
